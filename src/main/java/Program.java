f4b5489ad6f5aa0ac5e82c2799d1829f852100ac
Pycuk <kha4etlov.ruslan@yandex.ru>
2023-09-09 23:27:22 +0300

    Program

---
+++ src/main/java/Program.java
@@ -0,0 +1,10 @@
+
+import javax.swing.*;
+
+public class Program {
+
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        ProgramFrame programFrame = new ProgramFrame();
+
+    }
+}
