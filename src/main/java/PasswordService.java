11336ea8f69afcc3cb7697dcda530daabac4986f
Pycuk <kha4etlov.ruslan@yandex.ru>
2023-09-10 14:07:18 +0300

    Класс для работы с паролями

--- src/main/java/PasswordService.java
+++ src/main/java/PasswordService.java
@@ -1,15 +1,34 @@
 import java.security.SecureRandom;
 
+/**
+ * Класс для работы с паролями
+ *
+ * @autor Pycukvsu
+ */
 public class PasswordService {
     private StringBuilder alphabet;
-    private SecureRandom secureRandom = new SecureRandom();
+    private final SecureRandom secureRandom = new SecureRandom();
     private StringBuilder password;
 
+    /**
+     * Метод генерирует рандомные пароли.
+     * Также метод обрабатывает то, чтобы символы пароля не повторялись.
+     * Заданая длина пароля не может быть больше длины алфавита.
+     *
+     * @param length - длина пароля
+     * @param numbers - будет ли пароль содержать цифры
+     * @param punctuationMarks - будет ли содержать пароль специальные символы
+     * @param upperCase - будет ли пароль содержать буквы верхнего регистра
+     *
+     * @return password - получившийся пароль
+     */
     public String generatePassword(int length, boolean numbers,
                                    boolean punctuationMarks, boolean upperCase) {
         password = new StringBuilder();
         alphabet = new StringBuilder();
 
+        // Добавление в алфавит сиволов
+        // Нижний регистр по дефолту добавляется
         alphabet.append("abcdefghijklmnopqrstuvwxyz");
         if (numbers) {
             alphabet.append("0123456789");
@@ -21,9 +40,19 @@ public class PasswordService {
             alphabet.append("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
         }
 
+        if (length > alphabet.length()){
+            return "Уменьшите длину пароля";
+        }
+
+        // Обработка повторяющихся символов
+        // Индексы сохраняются в replay
         StringBuilder replay = new StringBuilder();
         for (int i = 0; i < length; i++) {
             int randomIndex = secureRandom.nextInt(alphabet.length());
+
+            // Если randomIndex не содержится в replay
+            // то добавляем по этому индексу символ из alphabet в password
+            // и добавляем randomIndex в replay
             if (!replay.toString().contains(Integer.toString(randomIndex))) {
                 password.append(alphabet.charAt(randomIndex));
                 replay.append(randomIndex);
