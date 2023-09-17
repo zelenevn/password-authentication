7a95bd1f45d28299cfab70d9ebe1c37a9629d595
Pycuk <kha4etlov.ruslan@yandex.ru>
2023-09-17 12:31:47 +0300

    Тест

--- src/test/java/domain/PasswordGeneratorImplTest.java
+++ src/test/java/domain/PasswordGeneratorImplTest.java
@@ -1,22 +1,24 @@
 package domain;
 
+import domain.generators.PasswordGenerator;
+import domain.generators.PasswordGeneratorImpl;
 import org.junit.jupiter.api.Test;
 import org.testng.Assert;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PasswordGeneratorTest {
-    PasswordBuilder passwordBuilder = new PasswordGenerator();
+class PasswordGeneratorImplTest {
+    PasswordGenerator passwordGenerator = new PasswordGeneratorImpl();
 
     @Test
     void length() {
-        Assert.assertEquals(10, passwordBuilder.generatePassword(10, false, false,
+        Assert.assertEquals(10, passwordGenerator.generatePassword(10, false, false,
                 false).length());
     }
 
     @Test
     void repeatingCharacters(){
-        String password = passwordBuilder.generatePassword(10, false, false, false);
+        String password = passwordGenerator.generatePassword(10, false, false, false);
 
         for (int i = 1; i < password.length() - 1; i++) {
             String symbol = String.valueOf(password.charAt(i));
