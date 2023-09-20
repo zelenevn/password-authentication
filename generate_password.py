3146a459006bdb8e17e2b1581d8020cccd455320
dariagon101 <dasha.goncharova.0303@mail.ru>
2023-09-20 14:34:33 +0300

    add interface

--- generate_password.py
+++ generate_password.py
@@ -47,7 +47,22 @@ def generate_password(len_pass: int, num_selected_groups: list):
     )
 
 
-if __name__ == '__main__':
+def interface():
+    len_pass_str = input("Введите желаему длину пароля: ")
+    len_pass = int(len_pass_str)
 
-    for i in range(10):
-        print(generate_password(len_pass=10, num_selected_groups=[1, 2, 3]))
+    num_selected_groups = []
+    for element in input("""Введите через пробел желаемые алфавиты для пароля:
+                        0: a-z,
+                        1: A-Z,
+                        2: 0-9
+                        3: спец. смволы
+                        """).split():
+        num_selected_groups.append(int(element))
+
+    return len_pass, num_selected_groups
+
+
+if __name__ == '__main__':
+    len_pass, num_selected_groups = interface()
+    print("Пароль: " + generate_password(len_pass, num_selected_groups))
