05e9c508491ab86f8f551afff9989c0975e13755
Владимир Коваленко <phoekoby@mail.ru>
2023-09-24 23:56:31 +0300

    Добавил файлы

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import GeneratePasswordFrom from "./components/GeneratePasswordFrom";
+
+function App() {
+  return (
+    <div className="App">
+      <GeneratePasswordFrom/>
+    </div>
+  );
+}
+
+export default App;
