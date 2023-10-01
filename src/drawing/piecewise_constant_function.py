00b372efd1bf335e2f65e0e17cd67ee00c8694b1
KonstantinElfimow <k.elfimow@gmail.com>
2023-10-01 10:47:36 +0300

    Построил гистограмму скорости ввода парольной фразы.

--- src/drawing/piecewise_constant_function.py
+++ src/drawing/piecewise_constant_function.py
@@ -1,28 +1,39 @@
-import math
-
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 def draw(intervals: list[float], holdings_time: list[float]):
+    print('intervals: ', intervals)
+    print('holding_time: ', holdings_time)
     # суммируем все значения из первого списка
-    x_sum = sum(intervals) + holdings_time[-1]
+    x_sum = sum(intervals)
 
     # создаем список координат x и y для построения графика
     x_coords = []
     y_coords = []
     for i in range(len(holdings_time)):
         if i == 0:
-            x_coord = sum(intervals[:i])  # откладываем i-ое значение из первого списка
+            x_coord = 0
             y_coord = 0
         else:
-            x_coord = 0
-            y_coord = intervals[i - 1]  # создаем список длины i-го элемента из второго списка
+            x_coord = sum(intervals[:i - 1])
+            y_coord = intervals[i - 1]
         x_coords.append(x_coord + holdings_time[i])
         y_coords.append(y_coord)
 
-    # строим график
-    plt.hist(x_coords, bins=np.arange(0, x_sum, x_sum / len(holdings_time)), align='left')
-    plt.xlabel('Отчёт времени')
-    plt.ylabel('Номер элемента')
+    # задаем параметры для столбцов
+    width = 0.5
+    bottom = 0
+
+    # строим гистограмму
+    plt.bar(x_coords, y_coords, width=width, bottom=bottom)
+
+    # устанавливаем максимальную высоту гистограммы
+    plt.ylim((0, max(intervals)))
+    plt.minorticks_on()
+    plt.xlim((0, x_sum))
+    plt.grid(which='major')
+    plt.grid(which='minor', linestyle=':')
+
+    # Отображение графика
     plt.show()
