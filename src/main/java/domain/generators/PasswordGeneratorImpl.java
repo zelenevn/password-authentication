a7a2556b2969abe715e1b9ef867904b528c15490
Pycuk <kha4etlov.ruslan@yandex.ru>
2023-10-17 01:00:56 +0300

    Лабораторная №1

---
+++ src/main/java/domain/generators/PasswordGeneratorImpl.java
@@ -0,0 +1,122 @@
+package domain.generators;
+
+import domain.models.Alphabet;
+
+import java.security.SecureRandom;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Класс для работы с паролями
+ */
+public class PasswordGeneratorImpl implements PasswordGenerator {
+
+    private final SecureRandom secureRandom;
+    private Set<Integer> replayIndex;
+
+    public PasswordGeneratorImpl() {
+        this.secureRandom = new SecureRandom();
+    }
+
+    /**
+     * Метод генерирует рандомные пароли.
+     * Обработка повторов в пароле.
+     * @param length - длина пароля
+     * @return password - получившийся пароль
+     */
+    @Override
+    public String generatePassword(int length, Alphabet symbols) {
+        StringBuilder alphabet = symbols.getSymbols();
+        byte[] password = new byte[length];
+        replayIndex = new HashSet<>();
+
+        if (length > alphabet.length()){
+            return "Укажите длину поменьше";
+        }
+
+        // Обработка повторяющихся символов
+        // Индексы сохраняются в replay
+        int lastIndex = 0;
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(alphabet.length());
+
+            // Если randomIndex не содержится в replay
+            // то добавляем по этому индексу символ из alphabet в password
+            // и добавляем randomIndex в replay
+            if (!viewReplays(randomIndex)) {
+                password[lastIndex] = (byte) alphabet.charAt(randomIndex);
+                lastIndex++;
+            } else {
+                length++;
+            }
+        }
+
+        return new String(password);
+    }
+
+    /**
+     * Метод проверяет наличие повторных индексов.
+     *
+     * @param index - индекс элемента
+     * @return  false - такой элемент есть | true - такого элемента нет
+     */
+    private boolean viewReplays(int index){
+        int lastSize = replayIndex.size();
+        replayIndex.add(index);
+        return replayIndex.size() != lastSize + 1;
+    }
+
+    /**
+     * Метод проверяет наличие цифр в пароле.
+     *
+     * @param password - пароль
+     * @return  false - цифр в пароле нет | true - цифры в пароле есть
+     */
+    @Override
+    public boolean containNumbers(String password){
+        for (int i = 48; i < 58; i++) {
+            char number = (char) i;
+            if (password.contains(String.valueOf(number))){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Метод проверяет наличие букв верхнего регистра в пароле.
+     *
+     * @param password - пароль
+     * @return false - буквы верхнего регистра в пароле нет | true - буквы верхнего регистра в пароле есть
+     */
+    @Override
+    public boolean containUpperCase(String password){
+        for (int i = 65; i < 91; i++) {
+            char number = (char) i;
+            if (password.contains(String.valueOf(number))){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Метод проверяет наличие специальных символов в пароле.
+     *
+     * @param password - пароль
+     * @return false - специальных символов в пароле нет | true - специальные символы в пароле есть
+     */
+    @Override
+    public boolean containPunctuationMarks(String password){
+        for (int i = 33; i < 48; i++) {
+            char number = (char) i;
+            if (password.contains(String.valueOf(number))){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/domain/generators/PasswordGeneratorImplTest.java
@@ -0,0 +1,58 @@
+package domain.generators;
+
+
+import domain.models.Alphabet;
+import org.junit.jupiter.api.Test;
+
+import org.testng.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class PasswordGeneratorImplTest {
+    PasswordGenerator passwordGenerator = new PasswordGeneratorImpl();
+
+    @Test
+    void length() {
+        int lengthPassword = passwordGenerator.generatePassword(10, new Alphabet()).length();
+        Assert.assertEquals(10, lengthPassword);
+    }
+
+    @Test
+    void repeatingCharacters() {
+        String password = passwordGenerator.generatePassword(10, new Alphabet());
+
+        for (int i = 1; i < password.length() - 1; i++) {
+            String symbol = String.valueOf(password.charAt(i));
+            String[] passwordSplit = password.split(symbol);
+            assertFalse(passwordSplit[0].contains(symbol));
+            assertFalse(passwordSplit[1].contains(symbol));
+        }
+    }
+
+    @Test
+    void presenceNumbers() {
+        String passwordContainNumbers = "aaaaa1aa";
+        String passwordNotContainNumbers = "aaaaaaaa";
+
+        Assert.assertTrue(passwordGenerator.containNumbers(passwordContainNumbers));
+        Assert.assertFalse(passwordGenerator.containNumbers(passwordNotContainNumbers));
+    }
+
+    @Test
+    void presenceUpperCase() {
+        String passwordContainUpperCase = "aaaaaAaa";
+        String passwordNotContainUpperCase = "aaaaaaaa";
+
+        Assert.assertTrue(passwordGenerator.containUpperCase(passwordContainUpperCase));
+        Assert.assertFalse(passwordGenerator.containNumbers(passwordNotContainUpperCase));
+    }
+
+    @Test
+    void presencePunctuationMarks() {
+        String passwordContainMarks = "aaaaa,aa";
+        String passwordNotContainMarks = "aaaaaaaa";
+
+        Assert.assertTrue(passwordGenerator.containPunctuationMarks(passwordContainMarks));
+        Assert.assertFalse(passwordGenerator.containPunctuationMarks(passwordNotContainMarks));
+    }
+}
